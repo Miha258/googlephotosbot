0023e1736f7dc2cd431dbef555fc6c953238e296
Miha258 <77239046+Miha258@users.noreply.github.com>
2022-06-02 12:54:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,17 @@
 from configparser import RawConfigParser
 import os
 
-thisfolder = os.path.join(os.path.dirname(os.path.abspath(__file__)),'server')
+thisfolder = os.path.dirname(os.path.abspath(__file__))
 initfile = os.path.join(thisfolder, 'server.config')
 config = RawConfigParser()
 res = config.read(initfile)
 
 def server_port():
-    return config.get('serverconfig','PORT')
+    return config.get('SERVER','PORT')
 
 def server_host():
-    return config.get('serverconfig','HOST')
+    return config.get('SERVER','HOST')
+
+
+
+
